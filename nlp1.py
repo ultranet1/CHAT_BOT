228b93abab357941604734d2648ee0f3cae2acaa
ALARAN OLUWADAMILARE IBRAHIM <65920846+ultranet1@users.noreply.github.com>
2021-02-09 17:00:46 +0100

    Update nlp1.py

--- nlp1.py
+++ nlp1.py
@@ -8,27 +8,14 @@ def get_text():
     input_text = st.text_input("You: ","So, what's in your mind")
     return input_text 
 
-import base64
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
-set_png_as_page_bg('ch1.jpg')
-
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://github.com/ultranet1/CHAT_BOT/blob/main/ch1.jpg");
+background-size: cover;
+}
+</style>
+'''
 
 ai= 'ai.yml'
 
