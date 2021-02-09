e4273e42169d98e89118960a2427343180b8448e
ALARAN OLUWADAMILARE IBRAHIM <65920846+ultranet1@users.noreply.github.com>
2021-02-09 14:11:33 +0100

    Update nlp1.py

--- nlp1.py
+++ nlp1.py
@@ -36,12 +36,14 @@ ai= r'‪C:\Users\HP\archive (1)\greetings.yml'
 
 
 st.sidebar.markdown(f"<h1 style='text-align: center; color: blue;'> ULTRA BOT</h1>", unsafe_allow_html=True)
-st.subheader(""" 
- ULTRA BOT is an NLP conversational chatterbot aimed at helping business to answer FAQs from customers. Initialize the bot by clicking the "Start Chat" button. 
-""")
+
 
 
 st.markdown("<h1 style='text-align: center; color: white;'>ULTRA BOT</h1>", unsafe_allow_html=True)
+
+st.subheader(""" 
+ ULTRA BOT is an NLP conversational chatterbot aimed at helping business to answer FAQs from customers. Initialize the bot by clicking the "Start Chat" button. 
+""")
 
 ind = 1
 if st.sidebar.button('Start Chatting'):
